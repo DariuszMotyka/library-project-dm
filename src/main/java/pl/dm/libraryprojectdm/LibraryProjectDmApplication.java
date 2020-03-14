9d7690c4786793c966864c41606b05748c1a47a5
Padre <dmotyka1990@gmail.com>
2020-03-14 10:48:22 +0100

    Initial commit

---
+++ src/main/java/pl/dm/libraryprojectdm/LibraryProjectDmApplication.java
@@ -0,0 +1,13 @@
+package pl.dm.libraryprojectdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectDmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryProjectDmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dm/libraryprojectdm/LibraryProjectDmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dm.libraryprojectdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectDmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
