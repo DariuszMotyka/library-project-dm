46230adc521333baf96d00d302f02fe0ee8af4f1
Padre <dmotyka1990@gmail.com>
2020-03-14 11:26:07 +0100

    Create Author and Book model.

---
+++ src/main/java/pl/dm/libraryprojectdm/model/Author.java
@@ -0,0 +1,11 @@
+package pl.dm.libraryprojectdm.model;
+
+public class Author {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+}
---
+++ src/main/java/pl/dm/libraryprojectdm/model/Book.java
@@ -0,0 +1,16 @@
+package pl.dm.libraryprojectdm.model;
+
+import java.time.LocalDate;
+
+public class Book {
+
+    private Long id;
+
+    private String title;
+
+    private LocalDate releaseDate;
+
+    private String isbn;
+
+    private Author author;
+}
